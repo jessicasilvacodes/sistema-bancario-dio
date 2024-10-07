5a3a91f0bedbb8a29f32ef0f816063c7e9e3b63f
Jessica Silva <jessicasilvacode@gmail.com>
2024-10-07 17:01:37 -0300

    commit -m "entendendo a versao 2 do sistema"

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
 ## "Criando um Sistema Bancário com Python"
 
-### Desafio 02 do bootcamp "NTT DATA - Engenharia de Dados com Python" da [DIO (Digital Innovation One)](https://web.dio.me/home)
+### Desafio do bootcamp "NTT DATA - Engenharia de Dados com Python" da [DIO (Digital Innovation One)](https://web.dio.me/home)
 
-### Entendendo o desafio:
-
-Um grande banco deseja modernizar suas operações e desenvolver seu novo sistema na linguagem Python. 
-
-Para a primeira versão do sistema (v1), devemos implementar as seguintes operações: **depósito, saque e extrato**.
-
-Nas operações de depósito, deve ser possível depositar valores positivos. *A v1 do projeto trabalha apenas com 1 usuário, dessa forma, por enquanto, não precisaremos nos preocupar com identificador pessoal, número de agência e conta bancária*. Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
-
-O sistema deve permitir realizar 3 saques diários com limite máximo de R$500,00 por saque. Caso o usuário não tenha dinheiro em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
-
-As operações de extrato devem listar todos os depósitos e saques realizados na conta. No fim da listagem, deve ser exibido o saldo atual. Os valores devem ser exibidos no formato: R$xxxx.xx
-
+### em andamento e em construção ...  
 
 .
 .
--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -1,10 +1,23 @@
+'''
+Entendendo o desafio:
+
+Um grande banco deseja modernizar suas operações e desenvolver seu novo sistema na linguagem Python.
+
+Para a primeira versão do sistema (v1), devemos implementar as seguintes operações: depósito, saque e extrato.
+
+Nas operações de depósito, deve ser possível depositar valores positivos. A v1 do projeto trabalha apenas com 1 usuário, dessa forma, por enquanto, não precisaremos nos preocupar com identificador pessoal, número de agência e conta bancária. Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+O sistema deve permitir realizar 3 saques diários com limite máximo de R$500,00 por saque. Caso o usuário não tenha dinheiro em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+As operações de extrato devem listar todos os depósitos e saques realizados na conta. No fim da listagem, deve ser exibido o saldo atual. Os valores devem ser exibidos no formato: R$xxxx.xx
+'''
 
 menu = """
 
-[0] Depositar
-[1] Sacar
-[2] Visualizar o extrato
-[3] Sair do sistema
+[d] Depositar
+[s] Sacar
+[e] Visualizar o extrato
+[q] Sair do sistema
 
 => """
 
@@ -19,7 +32,7 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == "0":      #depositar
+    if opcao == "d":      #depositar
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:      #evitar depósitos de valores negativos
@@ -29,7 +42,7 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "1":      #sacar
+    elif opcao == "s":      #sacar
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo      #verificar se excedeu o saldo
@@ -55,12 +68,12 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "2":      #visualizar o extrato
+    elif opcao == "e":      #visualizar o extrato
         print("\nEXTRATO: ")
         print("Não foram realizadas movimentações." if not extrato else extrato)      #se não foram feitas operações
         print(f"\nSaldo: R$ {saldo:.2f}")
 
-    elif opcao == "3":      #sair do sistema
+    elif opcao == "q":      #sair do sistema
         break
 
     else:
